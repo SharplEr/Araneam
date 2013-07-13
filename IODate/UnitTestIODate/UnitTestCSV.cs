78354de90afd936843c838d121df59f9199728b2
Sharpler <sharplermc@gmail.com>
2013-07-13 14:17:21 +0400

    Some remove and clean up.
    Signed-off-by: Sharpler <sharplermc@gmail.com>

---
+++ IODate/UnitTestIODate/UnitTestCSV.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IODate;
+using System.IO;
+
+namespace UnitTestIODate
+{
+    [TestClass]
+    public class UnitTestCSV
+    {
+        [TestMethod]
+        public void TestCSVRead()
+        {
+            const string name = @".\xyu.csv";
+            const string some1 = @"LSD,Кавай,MDMA";
+            const string some2 = @"A,B,AB";
+            StreamWriter writer = new StreamWriter(name);
+
+            writer.WriteLine(some1);
+            writer.WriteLine(some2);
+            writer.Close();
+
+            CSVReader reader = null;
+
+            try
+            {
+                reader = new CSVReader(name);
+            }
+            catch (NullReferenceException e)
+            {
+                Assert.Fail("Сообщение: <" + e.Message + ">. Источник: <" + e.Source + ">. Стек вызовов: <" + e.StackTrace + ">");
+            }
+
+            try
+            {
+                Assert.IsTrue((reader[0, "LSD"] == "A") && (reader[0, "Кавай"] == "B") && (reader[0, "MDMA"] == "AB"), "Неверно считало строку");
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Assert.Fail("Сообщение: <" + e.Message + ">. Источник: <" + e.Source + ">. Стек вызовов: <" + e.StackTrace + ">");
+            }
+        }
+
+        [TestMethod]
+        public void TestCSVReadTestCorrect()
+        {
+            const string name = @".\xyu.csv";
+            const string some1 = @"LSD,Кавай,MDMA";
+            const string some2 = @"A,""2,5"",AB";
+            StreamWriter writer = new StreamWriter(name);
+
+            writer.WriteLine(some1);
+            writer.WriteLine(some2);
+            writer.Close();
+
+            CSVReader reader = new CSVReader(name);
+            Assert.IsTrue(reader.Test(), "Определяет как некорректный корректный файл");
+        }
+
+
+        [TestMethod]
+        public void TestCSVReadTestWrong()
+        {
+            const string name = @".\xyu.csv";
+            const string some1 = @"LSD,Кавай,MDMA";
+            const string some2 = @"A,B,AB, Ad";
+            const string some3 = @"A,""2,5""";
+            StreamWriter writer = new StreamWriter(name);
+
+            writer.WriteLine(some1);
+            writer.WriteLine(some2);
+            writer.WriteLine(some3);
+            writer.Close();
+
+            CSVReader reader = new CSVReader(name);
+            Assert.IsFalse(reader.Test(), "Определяет как корректный некорректный файл");
+        }
+    }
+}
--- IODate/UnitTestIODate/UnitTestStatist.cs
+++ IODate/UnitTestIODate/UnitTestStatist.cs
@@ -1,48 +1,46 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Araneam;
+using IODate;
 using VectorSpace;
 
-
-namespace UnitTestNetwork
+namespace UnitTestIODate
 {
-    /*
     [TestClass]
     public class UnitTestStatist
     {
         [TestMethod]
-        public void TestGetRandomIndex()
+        public void TestStatistGetRandomIndex()
         {
             const int n = 13;
             int[] indexes = Statist.getRandomIndex(n);
-            
-            Assert.IsTrue(indexes.Length == n, "Длина массива" + ((indexes.Length>n)?"больше":"меньше") +"чем надо");
+
+            Assert.IsTrue(indexes.Length == n, "Длина массива" + ((indexes.Length > n) ? "больше" : "меньше") + "чем надо");
 
             bool[] count = new bool[n];
 
-            for(int i = 0; i< n; i++)
+            for (int i = 0; i < n; i++)
                 count[i] = false;
 
             try
             {
-            for(int i = 0; i< n; i++)
-                count[indexes[i]] = !count[indexes[i]];
+                for (int i = 0; i < n; i++)
+                    count[indexes[i]] = !count[indexes[i]];
             }
-            catch(IndexOutOfRangeException)
+            catch (IndexOutOfRangeException)
             {
                 Assert.Fail("Выход за пределы индексации");
             }
 
             bool flag = true;
 
-            for(int i = 0; i< n; i++)
-                flag = flag &&count[i];
+            for (int i = 0; i < n; i++)
+                flag = flag && count[i];
 
             Assert.IsTrue(flag, "Повторы номеров");
         }
 
         [TestMethod]
-        public void TestNormalization()
+        public void TestStatistNormalization()
         {
 
             Vector[] matrix = new Vector[2];
@@ -53,18 +51,17 @@ namespace UnitTestNetwork
             matrix[1][0] = -1;
             matrix[1][1] = 4;
             Action<Vector> f = matrix.Normalization();
-            
+
             Assert.AreEqual(matrix[0][0], 1.0, "Не нормализует массив");
             Assert.AreEqual(matrix[0][1], -1.0, "Не нормализует массив");
             Assert.AreEqual(matrix[1][0], -1.0, "Не нормализует массив");
             Assert.AreEqual(matrix[1][1], 1.0, "Не нормализует массив");
-            
+
             Vector some = new Vector(2);
             some[0] = 0.5;
             some[1] = 2.5;
             f(some);
-            Assert.IsTrue((some[0]==0.5)&&(some[1]==-0.5), "Возвращает функцию которая, не нормализует");
+            Assert.IsTrue((some[0] == 0.5) && (some[1] == -0.5), "Возвращает функцию которая, не нормализует");
         }
     }
-     * */
 }
